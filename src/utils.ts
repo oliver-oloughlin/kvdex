2f1145a37f7e3e7fbb57f292a9058aaf03296ce6
oliver-oloughlin <oliveroloughlin@hotmail.com>
2023-05-08 23:16:39 +0200

    Documentation for flatten function

--- src/utils.ts
+++ src/utils.ts
@@ -19,6 +19,13 @@ export async function useKV<T>(fn: (kv: Deno.Kv) => Promise<T>) {
   return result
 }
 
+/**
+ * Flattens a document at the top level, placing the id and versionstamp together with the document data.
+ * Only applicable to documents of KvObject data.
+ * 
+ * @param document - The document to flatten.
+ * @returns A flattened document with the document data, id and versionstamp.
+ */
 export function flatten<T extends KvObject>(document: Document<T>) {
   const { value, ...rest } = document
   return {
