f25f97426ab36f5ac5868f64fad5c628ae5d8f59
oliver-oloughlin <oliveroloughlin@hotmail.com>
2023-05-10 12:06:04 +0200

    init dev branch, first work on IndexableCollection

---
+++ src/indexable_collection.ts
@@ -0,0 +1,30 @@
+import { Collection, type CollectionKey } from "./collection.ts"
+import type { KvObject } from "./kvdb.types.ts"
+import { generateId, getDocumentKey, useKV } from "./kvdb.utils.ts"
+
+export type IndexRecord<T extends KvObject> = Partial<{
+  [key in keyof T]: true
+}>
+
+export class IndexableCollection<const T extends KvObject> extends Collection<T> {
+
+  private indexRecord?: IndexRecord<T>
+
+  constructor(collectionKey: CollectionKey, indexRecord?: IndexRecord<T>) {
+    super(collectionKey)
+    this.indexRecord = indexRecord
+  }
+
+  async add(data: T) {
+    return await useKV(async kv => {
+      const id = generateId()
+      const idKey = getDocumentKey(this.collectionKey, id)
+      let atomic = kv.atomic()
+
+      
+
+      return await atomic.commit()
+    })
+  }
+
+}
