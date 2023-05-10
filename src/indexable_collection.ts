c6fc513bb91686496f770ac35e9bda8d74545f56
oliver-oloughlin <oliveroloughlin@hotmail.com>
2023-05-10 12:09:57 +0200

    worked on indexable add method

--- src/indexable_collection.ts
+++ src/indexable_collection.ts
@@ -19,9 +19,15 @@ export class IndexableCollection<const T extends KvObject> extends Collection<T>
     return await useKV(async kv => {
       const id = generateId()
       const idKey = getDocumentKey(this.collectionKey, id)
-      let atomic = kv.atomic()
 
-      
+      let atomic = kv.atomic().set(idKey, data)
+
+      if (this.indexRecord) {
+        Object.keys(this.indexRecord).forEach(index => {
+          const indexValue = data[index]
+          const indexKey = getDocumentKey(this.collectionKey, indexValue)
+        })
+      }
 
       return await atomic.commit()
     })
