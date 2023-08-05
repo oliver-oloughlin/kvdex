4770462ce766c9edaab1b56018f5f26e3b263de6
h584974 <oliveroloughlin@hotmail.com>
2023-08-05 14:33:27 +0200

    db comments

--- src/db.ts
+++ src/db.ts
@@ -61,28 +61,54 @@ export function kvdex<const T extends SchemaDefinition>(
   }
 }
 
+/**
+ * Create a database schema from schema definition.
+ *
+ * @param def - Schema definition.
+ * @param kv - Deno KV instance.
+ * @param treeKey - The current tree key.
+ * @returns
+ */
 function _createSchema<const T extends SchemaDefinition>(
   def: T,
   kv: Deno.Kv,
   treeKey?: KvKey,
 ): Schema<T> {
+  // Get all the definition entries
   const entries = Object.entries(def)
 
+  // Create schema entries from schema definition entries
   const schemaEntries = entries.map(([key, value]) => {
+    // Get the current tree key
     const extendedKey = treeKey ? extendKey(treeKey, key) : [key] as KvKey
+
+    // If the entry value is a function => create collection builder context and run function.
     if (typeof value === "function") {
       const initializer = new CollectionBuilderContext(kv, extendedKey)
+
+      // Create and return collection entry
       return [key, value(initializer)]
     }
 
+    // Create and return schema entry
     return [key, _createSchema(value, kv, extendedKey)]
   })
 
+  // Create schema object from schema entries
   const schema = Object.fromEntries(schemaEntries)
 
+  // Return the built schema object
   return schema as Schema<T>
 }
 
+/**
+ * Count all documents in the KV store.
+ *
+ * @param kv - Deno KV instance.
+ * @param schemaOrCollection - Schema or Collection object.
+ * @param options - CountAll options.
+ * @returns Promise resolving to void the total count of documents in schema collections or collection.
+ */
 async function _countAll(
   kv: Deno.Kv,
   schemaOrCollection:
@@ -90,17 +116,28 @@ async function _countAll(
     | Collection<KvValue, CollectionDefinition<KvValue>>,
   options?: CountAllOptions,
 ): Promise<number> {
+  // If input is a collection, return the collection count
   if (schemaOrCollection instanceof Collection) {
     return await schemaOrCollection.count(options)
   }
 
+  // Recursively count the schema collections.
   const counts = await Promise.all(
     Object.values(schemaOrCollection).map((val) => _countAll(kv, val, options)),
   )
 
+  // Return the sum of collection counts
   return counts.reduce((sum, c) => sum + c, 0)
 }
 
+/**
+ * Delete all documents in the KV store.
+ *
+ * @param kv - Deno KV instance.
+ * @param schemaOrCollection - Schema or Collection object.
+ * @param options - DeleteAll options.
+ * @returns Promise resolving to void.
+ */
 async function _deleteAll(
   kv: Deno.Kv,
   schemaOrCollection:
@@ -108,11 +145,13 @@ async function _deleteAll(
     | Collection<KvValue, CollectionDefinition<KvValue>>,
   options?: DeleteAllOptions,
 ) {
+  // If input is a collection, delete all documents in the collection
   if (schemaOrCollection instanceof Collection) {
     await schemaOrCollection.deleteMany(options)
     return
   }
 
+  // Recursively delete all documents from schema collections
   await Promise.all(
     Object.values(schemaOrCollection).map((val) =>
       _deleteAll(kv, val, options)
@@ -120,19 +159,38 @@ async function _deleteAll(
   )
 }
 
+/**
+ * Enqueue data in the database queue.
+ *
+ * @param kv - Deno KV instance.
+ * @param data - Data to be enqueued.
+ * @param options - Enqueue options.
+ * @returns Promise resolving to void.
+ */
 async function _enqueue(kv: Deno.Kv, data: unknown, options?: EnqueueOptions) {
+  // Create a queue message without collection key
   const msg: QueueMessage = {
     collectionKey: null,
     data,
   }
 
+  // Enqueue the queue message with the kv instance
   return await kv.enqueue(msg, options)
 }
 
+/**
+ * Listen for data in the database queue.
+ *
+ * @param kv - Deno KV instance.
+ * @param handler - Data handler function.
+ */
 async function _listenQueue(kv: Deno.Kv, handler: QueueMessageHandler) {
+  // Listen for messages in the kv queue
   await kv.listenQueue(async (msg) => {
+    // Destruct the queue message
     const { collectionKey, data } = msg as QueueMessage
 
+    // If no collection key, invoke the handler for database queue data.
     if (!collectionKey) {
       await handler(data)
     }
