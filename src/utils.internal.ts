1f2e60dddb919fff78cf09041df9936e61169ce4
h584974 <oliveroloughlin@hotmail.com>
2023-08-05 14:04:00 +0200

    added comments for internal utils

--- src/utils.internal.ts
+++ src/utils.internal.ts
@@ -8,55 +8,102 @@ import type {
   Model,
 } from "./types.ts"
 
+/**
+ * Generate a random document id.
+ *
+ * @returns A generated id of type KvId.
+ */
 export function generateId() {
   return crypto.randomUUID()
 }
 
+/**
+ * Extend a kv key with key parts.
+ *
+ * @param key - The input key to be extended.
+ * @param keyParts - Key parts to add to the input key.
+ * @returns An extended kv key.
+ */
 export function extendKey(collectionKey: KvKey, ...keyParts: KvKey) {
   return [...collectionKey, ...keyParts] as KvKey
 }
 
+/**
+ * Get a document id from a document key.
+ *
+ * @param key - A document key.
+ * @returns A document id, or undefined if key is empty.
+ */
 export function getDocumentId(key: Deno.KvKey) {
   return key.at(-1)
 }
 
+/**
+ * Compare two kv keys for equality.
+ *
+ * @param k1 - First kv key.
+ * @param k2 - Second kv key.
+ * @returns true if keys are equal, false if not.
+ */
 export function keyEq(k1: KvKey, k2: KvKey) {
   return JSON.stringify(k1) === JSON.stringify(k2)
 }
 
+/**
+ * Determine whether a kv value is an instance of KvObject.
+ *
+ * @param value - A value of type KvValue.
+ * @returns true if the value is an instance of KvObject, false if not.
+ */
 export function isKvObject(value: KvValue) {
-  if (value !== null && typeof value === "object") {
-    if (
-      value instanceof Deno.KvU64 ||
-      value instanceof Array ||
-      value instanceof Int8Array ||
-      value instanceof Int16Array ||
-      value instanceof Int32Array ||
-      value instanceof BigInt64Array ||
-      value instanceof Uint8Array ||
-      value instanceof Uint16Array ||
-      value instanceof Uint32Array ||
-      value instanceof BigUint64Array ||
-      value instanceof Uint8ClampedArray ||
-      value instanceof Float32Array ||
-      value instanceof Float64Array ||
-      value instanceof ArrayBuffer ||
-      value instanceof Date ||
-      value instanceof Set ||
-      value instanceof Map ||
-      value instanceof RegExp ||
-      value instanceof DataView ||
-      value instanceof Error
-    ) {
-      return false
-    }
-
-    return true
+  // If value is null or undefined, return false
+  if (value === null || value === undefined) {
+    return false
   }
 
-  return false
+  // If value is not an object, return false
+  if (typeof value !== "object") {
+    return false
+  }
+
+  // If value is an instance of other KvValue objects, return false
+  if (
+    value instanceof Deno.KvU64 ||
+    value instanceof Array ||
+    value instanceof Int8Array ||
+    value instanceof Int16Array ||
+    value instanceof Int32Array ||
+    value instanceof BigInt64Array ||
+    value instanceof Uint8Array ||
+    value instanceof Uint16Array ||
+    value instanceof Uint32Array ||
+    value instanceof BigUint64Array ||
+    value instanceof Uint8ClampedArray ||
+    value instanceof Float32Array ||
+    value instanceof Float64Array ||
+    value instanceof ArrayBuffer ||
+    value instanceof Date ||
+    value instanceof Set ||
+    value instanceof Map ||
+    value instanceof RegExp ||
+    value instanceof DataView ||
+    value instanceof Error
+  ) {
+    return false
+  }
+
+  return true
 }
 
+/**
+ * Set document indices using an atomic operation.
+ *
+ * @param id - Document id.
+ * @param data - Document value.
+ * @param atomic - An initialized atomic operation.
+ * @param collection - The collection context.
+ * @returns The atomic operation with added mutations.
+ */
 export function setIndices<
   T1 extends Model,
   T2 extends IndexableCollectionDefinition<T1>,
@@ -106,6 +153,15 @@ export function setIndices<
   return op
 }
 
+/**
+ * Delete document indices using an atomic operation.
+ *
+ * @param id - Document id.
+ * @param data - Document value.
+ * @param atomic - An initialized atomic operation.
+ * @param collection - The collection context.
+ * @returns The atomic operation with added mutations.
+ */
 export function deleteIndices<
   T1 extends Model,
   T2 extends IndexableCollectionDefinition<T1>,
