3a2c714a0d839acea7bfe83cdb4c3f49af062bd2
Oliver O'Loughlin <oliveroloughlin@hotmail.com>
2024-10-31 20:42:44 +0100

    chore: moved v8 encoder into separate folder

---
+++ src/ext/encoding/v8/mod.ts
@@ -0,0 +1,3 @@
+export { v8Encoder } from "./v8_encoder.ts";
+export { v8Deserialize, v8Serialize } from "./utils.ts";
+export type * from "./types.ts";
---
+++ src/ext/encoding/v8/v8_encoder.ts
@@ -0,0 +1,23 @@
+import type { Encoder } from "../../../types.ts";
+import type { V8EncoderOptions } from "./types.ts";
+import { v8Deserialize, v8Serialize } from "./utils.ts";
+
+/**
+ * V8-encoder.
+ *
+ * Used for serializing and deserializing data as Uint8Array.
+ *
+ * Relies on `serialize()` and `deserialize()` from the node:v8 built-in module.
+ *
+ * @param options - V8 encoding options.
+ * @returns - An Encoder object.
+ */
+export function v8Encoder(options?: V8EncoderOptions): Encoder {
+    return {
+        serializer: {
+            serialize: v8Serialize,
+            deserialize: v8Deserialize,
+        },
+        compressor: options?.compressor,
+    };
+}
