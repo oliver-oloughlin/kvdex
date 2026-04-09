6ae2e2e06b756af088861f9ea4a4e8ab1b28e64a
Oliver O'Loughlin <oliveroloughlin@hotmail.com>
2026-04-09 14:45:34 +0200

    chore: add remaining collection multi-part id tests

--- tests/collection/watch.test.ts
+++ tests/collection/watch.test.ts
@@ -1,4 +1,4 @@
-import { assert } from "@std/assert";
+import { assert, assertEquals } from "@std/assert";
 import { mockUser1, mockUser2, mockUser3 } from "../mocks.ts";
 import { sleep, useDb } from "../utils.ts";
 import type { Document } from "../../mod.ts";
@@ -67,4 +67,79 @@ Deno.test("collection - watch", async (t) => {
       await promise;
     });
   });
+
+  await t.step(
+    "Should receive all document updates for multi-part id",
+    async () => {
+      await useDb(async (db) => {
+        const id: [string, number] = ["id", 1];
+        const n1 = 10;
+        const n2 = 20;
+        const n3 = 30;
+        const docs: (Document<number, [string, number]> | null)[] = [];
+
+        const { promise, cancel } = db.multi_part_id_nums.watch(id, (doc) => {
+          docs.push(doc as Document<number, [string, number]> | null);
+        });
+
+        await db.multi_part_id_nums.set(id, n1);
+        await sleep(500);
+        await db.multi_part_id_nums.set(id, n2, { overwrite: true });
+        await sleep(500);
+        await db.multi_part_id_nums.update(id, n3);
+        await sleep(500);
+        await db.multi_part_id_nums.delete(id);
+        await sleep(500);
+
+        assert(docs.some((doc) => doc?.value === n1));
+        assert(docs.some((doc) => doc?.value === n2));
+        assert(docs.some((doc) => doc?.value === n3));
+        assert(docs.some((doc) => doc === null));
+
+        await cancel();
+        await promise;
+      });
+    },
+  );
+
+  await t.step(
+    "Should not receive unrelated document updates for multi-part id",
+    async () => {
+      await useDb(async (db) => {
+        const id1: [string, number] = ["id1", 1];
+        const id2: [string, number] = ["id2", 2];
+        const n1 = 10;
+        const n2 = 20;
+        const n3 = 30;
+        let count = 0;
+        let n = -1;
+        let lastDoc: any;
+
+        const { promise, cancel } = db.multi_part_id_nums.watch(id1, (doc) => {
+          count++;
+          lastDoc = doc;
+          if (doc?.value) {
+            n = doc.value;
+          }
+        });
+
+        await db.multi_part_id_nums.set(id2, n1);
+        await sleep(500);
+        await db.multi_part_id_nums.set(id2, n2, { overwrite: true });
+        await sleep(500);
+        await db.multi_part_id_nums.update(id2, n3);
+        await sleep(500);
+        await db.multi_part_id_nums.delete(id2);
+        await sleep(500);
+
+        // Account for initial invocation
+        assertEquals(count, 1);
+        assertEquals(n, -1);
+        assert(lastDoc === null);
+
+        await cancel();
+        await promise;
+      });
+    },
+  );
 });
