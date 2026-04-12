06ee68a6923973be12437591905841b21f0328b5
oliver-oloughlin <oliver.oloughlin@gmail.com>
2026-04-13 01:47:36 +0200

    fix: remove TODO

--- tests/serialized_indexable_collection/updateManyBySecondaryOrder.test.ts
+++ tests/serialized_indexable_collection/updateManyBySecondaryOrder.test.ts
@@ -8,7 +8,6 @@ import {
 import { generateUsers, useDb } from "../utils.ts";
 import type { User } from "../models.ts";
 
-// TODO: fix update document deleting indices even when failing to update document
 Deno.test(
   "serialized_indexable_collection - updateManyBySecondaryOrder",
   async (t) => {
